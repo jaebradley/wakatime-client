17814cd34e0c1dedd4c1e12d718b67e7182450f1
Jae Bradley <jae.b.bradley@gmail.com>
2020-01-31 13:47:10 -0500

    fix(dependencies): update node version and dependencies (#75)
    ### Summary
    
    Update `npm` dependencies, update node version defined in `.nvmrc` (which will also define the node version used by Travis).

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,6 @@
 import babel from 'rollup-plugin-babel';
-import commonjs from 'rollup-plugin-commonjs';
-import resolve from 'rollup-plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import resolve from '@rollup/plugin-node-resolve';
 import localResolve from 'rollup-plugin-local-resolve';
 import filesize from 'rollup-plugin-filesize';
 import minify from 'rollup-plugin-babel-minify';
@@ -41,12 +41,10 @@ const config = {
     babel({ exclude: 'node_modules/**' }),
     localResolve(),
     resolve({
-      module: true,
-      jsnext: true,
-      main: true,
       preferBuiltins: true,
       browser: true,
       modulesOnly: true,
+      mainFields: 'browser',
     }),
     minify(),
     terser(),
